79166d1c90cf74e85b7c4924e17b76015b3d158e
meniem13 <meniemnasr84@gmail.com>
2025-12-19 22:29:07 +0200

    fas

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post("/p", (req, res) => {
 // Routes
 app.get("/api", async (req, res) => {
   res.cookie("test_cookie", "value"); // Example cookie for testing
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/sign");
   res.setHeader("Access-Control-Allow-Methods", "GET,  OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const query = "SELECT * FROM meniem";
